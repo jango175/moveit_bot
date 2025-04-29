f6b1d2f611c51afc8b70e8c5befd8b31150e6f1d
jango175 <jan.gorski175@gmail.com>
2025-04-29 14:22:56 +0200

    one joint working

--- fpga_bot_moveit_config/launch/warehouse_db.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("FPGA_bot", package_name="fpga_bot_moveit_config").to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
